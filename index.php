b1737fff2652e237330016738c7fc792fed5ab95
Sujeet Kumar <sujeetk69@gmail.com>
2019-12-23 17:34:05 +0000

    Implement search functionality

--- index.php
+++ index.php
@@ -7,67 +7,59 @@
 <!-- Page Content -->
 <div class="container">
 
-    <div class="row">
+  <div class="row">
 
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
+    <!-- Blog Entries Column -->
+    <div class="col-md-8">
 
-            <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
-            </h1>
+      <h1 class="page-header">
+        Page Heading
+        <small>Secondary Text</small>
+      </h1>
 
-            <?php
-                $get_posts_query = "SELECT * FROM posts";
-                if($posts = $mysqli->query($get_posts_query)) {
-                    while($post = $posts->fetch_assoc()) {
-            ?>
-                        <!-- Each Blog Post -->
-                        <h2>
-                            <a href='#'><?php echo $post['title'] ?></a>
-                        </h2>
-                        <p class='lead'>
-                            by <a href='index.php'><?php echo $post['author']?></a>
-                        </p>
-                        <p><span class='glyphicon glyphicon-time'></span> Posted on <?php echo $post['date']?></p>
-                        <hr>
-                        <img class='img-responsive' src='<?php echo $post['image'] ?>' alt=''>
-                        <hr>
-                        <p><?php echo $post['content']?></p>
-                        <a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>
-                        <hr>  
-            
-            <?php
-                    }
-                }
-            
-            ?>
-            
-            
-            <!-- First Blog Post -->
-<!--
-            <h2>
-                <a href="#">Blog Post Title</a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php">Start Bootstrap</a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-            <hr>
-            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
--->
+      <?php
+      $get_posts_query = "SELECT * FROM posts";
+      
+      /* 
+        If search was done, search in the tags 
+        TODO: Seach engine should search in title and author too
+      */
+      if(isset($_POST['search-submit'])) {
+        $search_key = $_POST['search-key'];
+        $get_posts_query .= " WHERE tags LIKE '%$search_key%'";
+      }
+      
+      if($posts = $mysqli->query($get_posts_query)) {
+        while($post = $posts->fetch_assoc()) {
+      ?>
+      <!-- Each Blog Post -->
+      <h2>
+        <a href='#'><?php echo $post['title'] ?></a>
+      </h2>
+      <p class='lead'>
+        by <a href='index.php'><?php echo $post['author']?></a>
+      </p>
+      <p><span class='glyphicon glyphicon-time'></span> Posted on <?php echo $post['date']?></p>
+      <hr>
+      <img class='img-responsive' src='<?php echo $post['image'] ?>' alt=''>
+      <hr>
+      <p><?php echo $post['content']?></p>
+      <a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>
+      <hr>  
 
-            
-        </div>
+      <?php
+        }
+      }
+
+      ?>
+
+    </div>
 
     <!-- Blog Sidebar Widgets Column -->
     <?php include "includes/sidebar.php"; ?>
 
-    </div>
-    <!-- /.row -->
+  </div>
+  <!-- /.row -->
 
-    <hr>
-<?php include "includes/footer.php"; ?>
+  <hr>
+  <?php include "includes/footer.php"; ?>
