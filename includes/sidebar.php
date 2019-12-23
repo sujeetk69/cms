93263466c4f561f7c763a3e5e30b9a5015fb61a1
Sujeet Kumar <sujeetk69@gmail.com>
2019-12-23 17:35:36 +0000

    Show blog categories on side bar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,57 +1,51 @@
 <div class="col-md-4">
 
-    <!-- Blog Search Well -->
-    <div class="well">
-        <h4>Blog Search</h4>
-        <div class="input-group">
-            <input type="text" class="form-control">
-            <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
-                    <span class="glyphicon glyphicon-search"></span>
-            </button>
-            </span>
-        </div>
-        <!-- /.input-group -->
-    </div>
+  <!-- Blog Search Well -->
+  <div class="well">
+    <h4>Blog Search</h4>
+    <form name="search-form" action="index.php" method="post">
+      <div class="input-group">      
+        <input name="search-key" type="text" class="form-control">
+        <span class="input-group-btn">
+          <button name="search-submit" class="btn btn-default" type="submit">
+            <span class="glyphicon glyphicon-search"></span>
+          </button>
+        </span>      
+      </div>
+    </form>
+    <!-- /.input-group -->
+  </div>
 
-    <!-- Blog Categories Well -->
-    <div class="well">
-        <h4>Blog Categories</h4>
-        <div class="row">
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
-        </div>
-        <!-- /.row -->
-    </div>
+  <!-- Blog Categories Well -->
+  <div class="well">
+    <h4>Blog Categories</h4>
+    <div class="row">
+      <div class="col-lg-6">
+        <ul class="list-unstyled">
+          <?php
+          $query = "SELECT * FROM categories";
+          if($result = $mysqli->query($query)) {
+            while($row = $result->fetch_assoc()) {
+              echo "<li>
+                    <a href='#'>{$row['cat_title']}</a>
+                            </li>";
+            }
+          } 
+          else {
+            echo "Query Error";
+          }
+          ?>         
+        </ul>
+      </div>
+      <!-- /.col-lg-6 -->      
+  </div>
+  <!-- /.row -->
+</div>
 
-    <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+<!-- Side Widget Well -->
+<div class="well">
+  <h4>Side Widget Well</h4>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+</div>
 
 </div>
