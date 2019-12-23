a1f889f6652698cc49578bb21dbcc01d33971b62
Sujeet Kumar <sujeetk69@gmail.com>
2019-12-23 11:57:28 +0000

    Make DB connection framework and rename html to php

---
+++ includes/db.php
@@ -0,0 +1,24 @@
+<?php
+/* 
+DB connection parameters
+TODO: Read from from some kind of confuguration or properties file 
+*/
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_password'] = "";
+$db['db_name'] = "cms";
+/* Define constants to eliminate any chance of accidental modification of these params */
+foreach($db as $param_name => $param_value) {
+    /* We will have contants like DB_NAME, DB_USER etc */
+    define(strtoupper($param_name), $param_value);
+}
+
+
+$connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if($connection) {
+    echo "DB Connected";
+}
+else {
+    echo "DB Connection Error";
+}
+?>
